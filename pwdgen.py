6e3c9fca982672ac5a0be48286dd52a01d135b32
Nyssa <20154654@tafe.wa.edu.au>
2025-08-28 13:51:28 +0800

    added simple UI

--- pwdgen.py
+++ pwdgen.py
@@ -1,11 +1,15 @@
+import sys
 import random
 
 # create random 10 letters password
 num_charts = 10
-password = ""
-for index in range(num_charts):
-    char_index = random.randint(0,25)
-    char_index += ord('a')
-    password += chr(char_index)
+if len(sys.argv)>1:
+    num_charts=int(sys.argv[1])  #take number from user input in command line
+
+    password = ""
+    for index in range(num_charts):
+        char_index = random.randint(0,25)
+        char_index += ord('a')
+        password += chr(char_index)
 
 print(password)
